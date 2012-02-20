7c2c587524f052c949fce0f5c08775b240c7d006
Michael Cetrulo <mail2samus@gmail.com>
2012-02-20 00:08:54 -0200

    assistance: crawling form-requests

--- scraping/scraping/spiders/parsers/assistance.py
+++ scraping/scraping/spiders/parsers/assistance.py
@@ -1,7 +1,8 @@
 import re, logging
 from urlparse import urljoin
+from datetime import datetime
 
-from scrapy.http import Request
+from scrapy.http import Request, FormRequest
 
 
 class ParseAssistance(object):
@@ -12,14 +13,35 @@ class ParseAssistance(object):
     def parse_treemap(self, response):
         asistsala_re = re.compile(r"window\.open\(\\'(abms2/asistsala/ConsAsistencia\.asp[^']*)\\',")
         urls = (urljoin(response.url, uri) for uri in asistsala_re.findall(response.body))
-        return (Request(uri, self.crawl_preform) for uri in urls)
+        return [Request(uri, self.crawl_preform) for uri in urls]
 
     def crawl_preform(self, response):
         cons_asistencia_re = re.compile(r'href="(ConsAsistenciaBrief\.asp[^"]*)"')
         urls = (urljoin(response.url, uri) for uri in cons_asistencia_re.findall(response.body))
-        return (Request(uri, self.prepare_form) for uri in urls)
+        return [Request(uri, self.prepare_form) for uri in urls]
 
     def prepare_form(self, response):
+        legislature = re.search(r'>Legislatura ([^<]*)<', response.body).group(1)
+
+        daterange_pattern = r'>Rango de asistencias disponibles para el Cuerpo en la Legislatura: (\d{2}/\d{2}/\d{4}) - (\d{2}/\d{2}/\d{4})<'
+        daterange = re.findall(daterange_pattern, response.body)[0]
+        post_args = {
+            'fecDesde': datetime.strptime(daterange[0], '%d/%m/%Y').strftime('%d%m%Y'),
+            'fecHasta': datetime.strptime(daterange[1], '%d/%m/%Y').strftime('%d%m%Y'),
+        }
+
+        hidden_input_pattern = r'<INPUT TYPE=HIDDEN[^<]* NAME="([^"]+)" VALUE="([^"]+)">'
+        post_args.update(
+            dict(re.findall(hidden_input_pattern, response.body))
+        )
+
+        form_uri_pattern = r'<FORM METHOD=POST ACTION="([^"]+)"'
+        form_url = urljoin(response.url, re.search(form_uri_pattern, response.body).group(1))
+
+        callback = lambda response: self.parse_form_result(response, legislature)
+        return FormRequest(url=form_url, formdata=post_args, callback=callback)
+
+    def parse_form_result(self, response, legislature=None):
         pass
 
 
