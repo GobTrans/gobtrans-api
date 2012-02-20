743ba5edceaf054cb7c0df682e85c79e2dee4780
Andrés Ambrois <andresambrois@gmail.com>
2012-02-19 23:28:36 -0200

    First implementation of Substitutes scraper

--- scraping/scraping/spiders/parsers/substitutes.py
+++ scraping/scraping/spiders/parsers/substitutes.py
@@ -1,21 +1,64 @@
+from datetime import datetime
 from BeautifulSoup import BeautifulSoup
 from scrapemark import scrape
+from scrapy.http import FormRequest
+from collections import defaultdict
+
+from scraping.items import SubstitutesItem
+
+date_fmt = '%d%m%Y'
 
 def parse(spider, resp):
+    return FormRequest(resp.url,
+                       formdata={
+                           'Fecha': datetime.today().strftime(date_fmt),
+                           'Cuerpo': resp.url[-1],
+                           'Integracion': 'S',
+                           'Desde': '15021985',
+                           'Hasta': datetime.today().strftime(date_fmt),
+                           'Dummy': datetime.today().strftime(date_fmt),
+                           'TipoLeg': 'Act',
+                           'Orden': 'Legislador',
+                       },
+                       callback=parse_list)
+
+def parse_list(resp):
     html = BeautifulSoup(resp.body).prettify()
 
-    res = scrape(
-    """{* 
-        <tr>
-            <td>
-                <a>{{ [sen].nombre }}</a>
-                {* <strong>({{ [sen].ref }})</strong> *}
-            </td>
-            <td>
-                <font>partido {{ [sen].partido }}</font>
-            </td>
-        </tr>
-    *}""",
-    html=html)
-    
-    print res
+    members = scrape(
+        """{* 
+            <tr>
+                <td>
+                    <a>{{ [res].name }}</a>
+                    {* <strong>({{ [res].ref }})</strong> *}
+                </td>
+                <td>
+                    <font>partido {{ [res].party }}</font>
+                </td>
+            </tr>
+        *}""",
+        html=html)['res']
+
+    refs = scrape(
+        """
+        {* <font>Sustituye al <a>{{ [res].name }}</a>{{ [res].why }}{* desde el {{ [res].from }}*}{* hasta el {{ [res].to }}*}</font>*}
+        """,
+        html=html)['res']
+
+
+    items = []
+    for info in members:
+        if 'ref' in info and info['ref'] is not None:
+            ref = int(info['ref'])
+            substitutes = refs[ref-1]
+        else:
+            substitutes = defaultdict(lambda: None)
+        items.append(SubstitutesItem(name=info['name'],
+                                     party=info['party'], 
+                                     chamber=resp.url[-1],
+                                     substitutes=substitutes['name'],
+                                     substitutes_from=substitutes['from'],
+                                     substitutes_to=substitutes['to'],
+                                     substitutes_why=substitutes['why']))
+
+    return items
