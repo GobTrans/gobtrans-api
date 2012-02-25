f27d690be8c99da96fb7042ea4dd32f0e7abef3e
Michael Cetrulo <mail2samus@gmail.com>
2012-02-25 01:45:27 -0200

    start_callbacks as dict and "enabled/disabled" as CLI arguments

--- scraping/scraping/spiders/parlamento.py
+++ scraping/scraping/spiders/parlamento.py
@@ -12,18 +12,35 @@ class ParlamentoSpider(BaseSpider):
         'www.parlamento.gub.uy',
         'www0.parlamento.gub.uy',
     )
-    start_callbacks = (
-        #('http://www0.parlamento.gub.uy/palacio3/p_mapaTree.asp', 'assistance'),
-        ('http://www0.parlamento.gub.uy/forms/IntCpo.asp?Cuerpo=S', 'substitutes'),
-        #('http://www0.parlamento.gub.uy/forms/IntCpo.asp?Cuerpo=D', 'substitutes'),
-        #('http://www0.parlamento.gub.uy/palacio3/legisladores_der.asp', 'parliamentaries'),
-    )
+    start_callbacks = {
+        'assistance': (
+            'http://www0.parlamento.gub.uy/palacio3/p_mapaTree.asp',
+        ),
+        'substitutes': (
+            'http://www0.parlamento.gub.uy/forms/IntCpo.asp?Cuerpo=S',
+            'http://www0.parlamento.gub.uy/forms/IntCpo.asp?Cuerpo=D',
+        ),
+        'parliamentaries': (
+            'http://www0.parlamento.gub.uy/palacio3/legisladores_der.asp',
+        ),
+    }
 
     def __init__(self, *args, **kwargs):
         super(ParlamentoSpider, self).__init__(*args, **kwargs)
-        if kwargs:
-            self.start_callbacks = [(url, callback) for url, callback in kwargs.items()]
+        if 'enable' in kwargs:
+            enabled = kwargs['enable'].split(',')
+            self.start_callbacks = dict(
+                (parser, urls) for parser, urls in self.start_callbacks.items() if parser in enabled
+            )
+        elif 'disable' in kwargs:
+            disabled = kwargs['disable'].split(',')
+            self.start_callbacks = dict(
+                (parser, urls) for parser, urls in self.start_callbacks.items() if parser not in disabled
+            )
 
     def start_requests(self):
-        for url, parser in self.start_callbacks:
-            yield Request(url, partial(getattr(parsers, parser).parse, self))
+        for parser, urls in self.start_callbacks.items():
+            callback = partial(getattr(parsers, parser).parse, self)
+            for url in urls:
+                yield Request(url, callback)
+
